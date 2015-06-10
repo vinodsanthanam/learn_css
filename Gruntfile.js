a4038cb787a95c6fe67cbcf553fae9524e743f5e
Vinod Santhanam <vinod.santhanam@thoughtworks.com>
2015-06-09 21:54:34 -0700

    moved css file to scss and added npm and grunt tasks

---
+++ Gruntfile.js
@@ -0,0 +1,22 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    sass: {
+      dist: {
+        files: {
+          'styles/main.css': 'scss/main.scss'
+        }
+      }
+    },
+    watch: {
+      css: {
+        files: 'scss/*.scss',
+        tasks: ['sass']
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.registerTask('default', ['watch']);
+}
